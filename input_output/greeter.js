97fea3e6d05737c0e574b67e6f4cc5d959ca9cb4
K. P. MacGregor <kpsmacgregor@gmail.com>
2021-08-05 03:05:33 -0700

    Finished input-output exercises.

---
+++ input_output/greeter.js
@@ -0,0 +1,4 @@
+let rlsync = require('readline-sync');
+let firstName = rlsync.question('What is your first name? ');
+let lastName = rlsync.question('What is your last name? ');
+console.log(`Hello, ${firstName} ${lastName}!`);
