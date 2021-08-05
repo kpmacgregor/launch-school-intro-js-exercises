cad3d30bf75ddab3e903d1779b03d1f292e92a02
K. P. MacGregor <kpsmacgregor@gmail.com>
2021-08-05 02:43:59 -0700

    Finished Variables exercises

---
+++ variables_exercises/age.js
@@ -0,0 +1,8 @@
+const age = 43;
+
+console.log(`You are ${age} years old.`);
+
+for (let i = 1; i < 5; ++i) {
+  console.log(`In ${10 * i} years, you will be ${age + 10 * i } years old.`);
+}
+
---
+++ variables_exercises/greeter.js
@@ -0,0 +1,7 @@
+const name = 'Victor';
+const times = ['Morning', 'Afternoon', 'Evening'];
+
+times.forEach(time => {
+  console.log(`Good ${time}, ${name}.`);
+});
+
